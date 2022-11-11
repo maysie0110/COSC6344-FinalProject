c49c738e53a1cc11335f2f84074ffc2eb0217d5b
GuoJiechang <463587571@qq.com>
2022-11-11 15:01:16 -0600

    Add slice button on inspector
    Add slice button on inspector

--- Assets/Interaction.cs
+++ Assets/Interaction.cs
@@ -10,20 +10,20 @@ public class Interaction : MonoBehaviour
     {
         if (OVRInput.Get(OVRInput.Button.One, OVRInput.Controller.RTouch))
         {
-            Debug.Log("Button One Pressed!");
+            //Debug.Log("Button One Pressed!");
             //Application.Quit();
             volumeRenderedObject.SetRenderMode(VolumeRenderMode.DirectVolumeRendering);
         }
         if (OVRInput.Get(OVRInput.Button.Two, OVRInput.Controller.RTouch))
         {
-            Debug.Log("Button Two Pressed!");
+            //Debug.Log("Button Two Pressed!");
             volumeRenderedObject.SetRenderMode(VolumeRenderMode.IsosurfaceRendering);
         }
 
         // returns a Vector2 of the primary thumbstick’s current state.
         // (X/Y range of -1.0f to 1.0f)
         Vector2 value = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick, OVRInput.Controller.RTouch);
-        Debug.Log(value.x);
+        //Debug.Log(value.x);
         volumeRenderedObject.SetVisibilityWindowMin(value.x);
     }
 }
